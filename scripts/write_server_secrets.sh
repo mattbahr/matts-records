0aea9137de5a813a025d472ac33f605b74666048
Matt Bahr <mattbahr1992@gmail.com>
2026-02-23 12:22:14 -0500

    update workflow
    Isolate unit and integration tests
    
    test redis connection
    
    try without auth
    
    add auth back
    
    remove redis command from docker compose
    
    use default cache config
    
    add back full workflow
    
    upgrade playwright
    
    update lock file

--- scripts/write_server_secrets.sh
+++ scripts/write_server_secrets.sh
@@ -5,3 +5,4 @@ echo "$BACKBLAZE_APP_KEY" > ./server/secrets/backblaze_app_key
 echo "$BACKBLAZE_KEY_ID" > ./server/secrets/backblaze_key_id
 echo "$MONGODB_BASIC_USERNAME" > ./server/secrets/mongodb_basic_username
 echo "$MONGODB_BASIC_PASSWORD" > ./server/secrets/mongodb_basic_password
+echo "$REDIS_PASSWORD" > ./server/secrets/redis_password
